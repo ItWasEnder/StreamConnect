b17a36ede8101e98a0eb3c5022544b7f3f018a34
Ender <endergamingfilms@gmail.com>
2024-02-02 16:10:28 -0800

    Working electron

--- src/server/tikfinity-server.cjs
+++ src/server/tikfinity-server.cjs
@@ -1,23 +1,22 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-// const express = require('express');
-
-import express, { type Express, type Request, type Response} from "express";
+const express = require('express');
 
 const expressPort = 8832;
-
 const expressApp = express();
 
-let server: any;
+let server;
 
 console.log('tikfinity-server.cjs loaded');
 
 function start() {
     console.log('Starting express server...');
 
-    expressApp.get('/api/sample', (req: Request, res: Response) => {
+    expressApp.get('/api/sample', (req, res) => {
         res.json({ message: 'Hello from Express!' });
     });
     
